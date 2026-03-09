ef84a239b87f5367d8bb52beda4267968bb44799
AltanMaryo <abdullahhasipaltan@posta.mu.edu.tr>
2026-03-09 11:54:55 +0300

    Add files via upload

---
+++ 2 March - 8 March exercises/Exercise1.cs
@@ -0,0 +1,10 @@
+﻿public Text goldText;
+public Text crystalText;
+public Text foodText;
+public Text woodText;
+public Text stoneText;
+
+void UpdateResource(Text resourceText, int amount)
+{
+    resourceText.text = "Amount: " + amount.ToString();
+}
